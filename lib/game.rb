2f38e05f1ea80aa072cd8045236db403b19afc2d
Nicholas, Anthony (AU - Melbourne) <anicholas@deloitte.com.au>
2018-09-16 18:40:17 +1000

    Put main method into game class to facilitate testing

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,8 @@
 #GAME CLASS: stores basic constants & error messages associated with game 
+
+
+require_relative('./robot')
+
 class Game
         attr_reader :positions, :directions, :commands, :permitted_command_lengths, :errortext
 
@@ -15,4 +19,99 @@ class Game
                         "out_of_bounds" => "Sorry, that command would put the robot off the table. Command Ignored."
 			}
         end
+
+
+	def run
+		#Instantiate robot and game     
+		robot = Robot.new
+
+		#Initial placement of robot on the board
+		loop do
+			inputString = gets.chomp
+			if is_valid_placement(inputString)
+				inputArray = get_input_array(inputString)
+				robot.place(inputArray[1].to_i, inputArray[2].to_i, inputArray[3])
+				robot.report
+				break
+			end
+		end
+
+		#Allow the robot to move in response to stdin commands
+		loop do
+			inputString = gets.chomp
+			if is_valid_input(inputString)
+				inputArray = get_input_array(inputString)
+
+				command = inputArray[0]
+
+				case command
+				when "MOVE"
+					robot.move
+				when "LEFT"
+					robot.left
+				when "RIGHT"
+					robot.right
+				when "REPORT"
+					robot.report
+				when "PLACE"
+					robot.place(inputArray[1].to_i, inputArray[2].to_i, inputArray[3])
+				end
+			end
+		end
+	end
+
+	# is_valid_input method
+	def is_valid_input(inputString)
+		inputArray = get_input_array(inputString)
+		len = inputArray.length
+		if !(len == 1 or len == 4)
+			puts @errortext["invalid_command"]
+			return false
+		elsif len == 1 and @commands[1..4].include?(inputArray[0])
+			return true
+		elsif len == 4 and @commands[0] == inputArray[0]
+			return is_valid_placement(inputString)
+		else
+			puts @errortext["invalid_command"]
+			return false
+		end
+	end
+
+	#get_input_array method
+	def get_input_array(inputString)
+		if inputString.split(" ").length == 1
+			return [inputString]
+		elsif inputString.split(" ").length == 2
+			return [inputString.split(" ")[0]] + inputString.split(" ")[1].split(",")
+		else
+			return nil
+		end
+	end
+
+	# is_valid_placement method
+	def is_valid_placement(input)
+		if input.split(" ").length != 2
+			puts @errortext["placement"]
+			return false
+		end
+		inputArray = [input.split(" ")[0]] + input.split(" ")[1].split(",")
+		if (inputArray[0] != "PLACE" || inputArray.length != 4)
+			puts @errortext["placement"]
+			return false
+		elsif !@positions.include?(inputArray[1]) || !@positions.include?(inputArray[2])
+			puts @errortext["placement_position"]
+			return false
+		elsif !@directions.include?(inputArray[3])
+			puts @errortext["placement_direction"]
+			return false
+		else
+			return true
+		end
+	end
+
+
+
+
+
+
 end
--- spec/toy-robot_spec.rb
+++ spec/toy-robot_spec.rb
@@ -1,10 +1,22 @@
-require 'toy-robot'
+#require 'init'
+require 'robot'
+require 'game'
 
 describe 'Toy-robot' do
+=begin
 	it "returns the position and orientation of the robot" do
 		robot = Robot.new
 		expect(... ).to eq("1,1,NORTH")
 
-		
-
+		allow(STDIN).to receive(:gets) { 'joe' }
+=end
+	it "takes user's name and returns it" do
+		puts "Hello world"
+=begin
+	utput = capture_standard_output { init }
+  		expect(output).to eq ""
+  		allow(STDIN).to receive(:gets) { 'joe' }
+  		expect(game.ask_for_name).to eq 'Joe'
+=end
+	end
 end
